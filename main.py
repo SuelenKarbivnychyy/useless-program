2c9692853b79c1905ba619acccd0f552f9ffe11f
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-01-09 18:48:15 -0500

    Implement the program to allow user keep talking until he wants to exit

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ def emoji_talk():
     """
     A silly project for people to talk through emoji 🤗"""
 
-    Sad_emoji = ["😮‍💨", "🫤", "😖", "🥺", "☹"]
+    Sad_emoji = ["😮‍💨", "😖", "🥺", "☹", "😩","😞"]
     random_emoji = ["👯‍♀️","💆‍♀️", "👩‍🍼", "👸", "🧐", "😆"]
+    happy= ["😚", "🤩", "😁", "😀", "🤣", "🤩"]
 
-    person1 = input("Base on the options above, tell me how are your feellings today?:\n-Happy\n-Sad\n-Whatever\n\n").title()
-    
-    if person1 == "Sad":
-        print(f"{random.choice(Sad_emoji)}")
-        print(f"computer says {random.choice(random_emoji)}")
+    lets_talk = True
+
+    while lets_talk:
+
+        person_mood = input("Base on the options bellow, tell me how are your feellings today?:\n-Happy\n-Sad\n-Fine or press any key to exit: \n\n").title()
+        
+        if person_mood == "Sad":
+            print(f"\nYou said: {random.choice(Sad_emoji)}")
+        elif person_mood == "Happy":
+            print(f"\nYou said: {random.choice(happy)}")
+        elif person_mood == "Fine":
+            print(f"\nYou said: {random.choice(random_emoji)}") 
+
+        else:
+            lets_talk = False       
+
+        print(f"computer says: {random.choice(random_emoji)}")
+
               
 emoji_talk()         
